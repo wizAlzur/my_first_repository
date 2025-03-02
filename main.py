55a61a1dc3e58cff39a40920e93199bbb7f9f4aa
Mikhail Khodzhaev <mkhodzhaev06@gmail.com>
2025-03-02 23:36:58 +0300

    Добавлена новая функция с использованием аннотации типов

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def circle_sq(r: int | float) -> int | float:
 
 def triangle_sq(o: int | float, h: int | float) -> int | float:
     return 0.5 * o * h
+    
+
+def pyranid_v(o_sq: int | float, h: int | float) -> float:
+    return (1 / 3) * o_sq * h
 
 
 print(circle_sq(1))
