272e53e37dd2425e954b1adcae2b06ec0555ff95
Mikhail Khodzhaev <mkhodzhaev06@gmail.com>
2025-03-02 23:22:03 +0300

    Добавлена новая функция с аннотацией типов Старой функции добавлена аннотация типов

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import math
 
 
-def circle_sq(r):
+def circle_sq(r: int | float) -> int | float:
     sq = math.pi * math.pow(r, 2)
     return sq
 
 
+def triangle_sq(o: int | float, h: int | float) -> int | float:
+    return 0.5 * o * h
+
+
 print(circle_sq(1))
+print(triangle_sq(5, 2))
